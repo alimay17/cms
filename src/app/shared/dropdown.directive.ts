1f6ba51133f828debf3e396983ed65ce69b1a20c
Alice Smith <alimay.smith@outlook.com>
2023-01-22 15:52:35 -0700

    implement dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener
+} from '@angular/core';
 
 @Directive({
   selector: '[cmsDropdown]'
 })
 export class DropdownDirective {
+  constructor() {}
 
-  constructor() { }
+  // properties
+  @HostBinding('class.open') isOpen = false;
 
+  // methods
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
