a75de8577bc247a57d2d786c70535836aa6b6738
Alice Smith <alimay.smith@outlook.com>
2023-02-15 12:45:53 -0700

    add new & update functions to contacts service

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -7,18 +7,23 @@ import { MOCKCONTACTS } from './MOCKCONTACTS';
   providedIn: 'root'
 })
 export class ContactService {
-  // constructors
-  constructor() {
-    this.contacts = MOCKCONTACTS;
-  }
-
-  // events
-  contactListChangedEvent = new Subject<Contact[]>();
 
   // properties
   contacts: Contact[] = [];
+  private maxContactId: number;
+  
+  // events
+  contactListChangedEvent = new Subject<Contact[]>();
+
+  // constructor
+  constructor() {
+    this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
+  }
 
   // methods
+
+  // getters
   getContacts(): Contact[] {
     return this.contacts.slice();
   }
@@ -32,6 +37,33 @@ export class ContactService {
     return null;
   }
 
+  // Create Update Delete
+  addContact(newContact: Contact){
+    if(!newContact){
+      return;
+    }
+    this.maxContactId++;
+    newContact.id = this.maxContactId.toString();
+
+    this.contacts.push(newContact);
+    this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+  updateContact(originalContact: Contact, newContact: Contact){
+    if(!originalContact || !newContact){
+      return;
+    }
+
+    let pos = this.contacts.indexOf(originalContact);
+    if(pos < 0){
+      return;
+    }
+
+    newContact.id = originalContact.id;
+    this.contacts[pos] = newContact;
+    this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
   deleteContact(contact: Contact) {
     if (!contact) {
       return;
@@ -42,5 +74,17 @@ export class ContactService {
     }
     this.contacts.splice(pos, 1);
     this.contactListChangedEvent.next(this.contacts.slice());
+  }
+
+    // internal helper methods
+  private getMaxId(): number {
+    let maxId = 0;
+    this.contacts.forEach(contact => {
+      let currentId = +contact.id;
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    });
+    return maxId;
   }
 }
